91fecfc419d517057e4a5654c64dec52ac3c515f
Connor Jones <ConnorLeeJones@gmail.com>
2019-05-24 21:07:25 -0400

    Fnished lab

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,49 @@
-
-
-
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String numString = "";
+        for (int x = 0; x < stop; x++)
+            numString += x;
+            return numString;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String numString = "";
+        for (int x = start; x < stop; x++)
+            numString += x;
+            return numString;
     }
 
-
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String numString = "";
+        for (int x = start; x < stop; x+= step)
+            numString += x;
+            return numString;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String numString = "";
+        for (int x = start; x < stop; x++)
+            if (x % 2 == 0){
+                numString += x;
+            } 
+            return numString;
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String numString = "";
+        for (int x = start; x < stop; x++)
+            if (x % 2 == 1){
+                numString += x;
+            } 
+            return numString;
     }
-
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
-    }
+        String numString = "";
+        for (int x = start; x <= stop; x++){
+            numString += (int)Math.pow(x, exponent);
+        }
+            return numString;
+        }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,37 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String triangle = "";
+        for (int i = 0; i < numberOfStars; i++)
+            triangle += "*";
+        return triangle;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        for (int i = 1; i <= numberOfRows; i++){
+            triangle += getRow(i);
+            triangle += "\n";
+        }
+        return triangle;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String triangle = "";
+        for (int i = 1; i <= 4; i++){
+            triangle += getRow(i);
+            triangle += "\n";
+        }
+        return triangle;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String triangle = "";
+        for (int i = 1; i <= 9; i++){
+            triangle += getRow(i);
+            triangle += "\n";
+        }
+        return triangle;
     }
 }
